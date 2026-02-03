384b08d52c09ca289cab2dfc16d0a9cbefd50159
lyan <lyankh02@gmail.com>
2026-02-03 16:06:49 +0300

    Initial Commit

---
+++ RYU/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RYU
+//
+//  Created by Lyan on 15/08/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RYU/RYUApp.swift
@@ -0,0 +1,17 @@
+//
+//  RYUApp.swift
+//  RYU
+//
+//  Created by Lyan on 15/08/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct RYUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
